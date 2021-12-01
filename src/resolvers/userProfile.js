fcafee25bd0d13b38350d4e93c3c7175ecd1d87a
husamaz1994 <11besehzulfiqar@seecs.edu.pk>
2021-12-01 17:48:41 +0500

    parent id adde to userProfile

--- src/resolvers/userProfile.js
+++ src/resolvers/userProfile.js
@@ -1,18 +1,33 @@
-import getFollows from "../utils/getFollows.js"
-import getProductsbyuserId from "../utils/getProductsbyuserId.js"
-export default {
-async follower(parent,args,context,info){
-    let followers=await getFollows(context,{userId:parent.userId,lookFor:parent.followerData});
-    return followers;
-},
-async following(parent,args,context,info){
-    let followers=await getFollows(context,{userId:parent.userId,lookFor:parent.followingData});
-    return followers;
-},
+import getFollows from "../utils/getFollows.js";
+import getProductsbyuserId from "../utils/getProductsbyuserId.js";
+import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
 
-async products(parent,args,context,info){
+export default {
+  async follower(parent, args, context, info) {
+    let followers = await getFollows(context, {
+      userId: parent.userId,
+      lookFor: parent.followerData,
+    });
+    return followers;
+  },
+  async following(parent, args, context, info) {
+    let followers = await getFollows(context, {
+      userId: parent.userId,
+      lookFor: parent.followingData,
+    });
+    return followers;
+  },
+
+  async products(parent, args, context, info) {
     console.log("follow products");
-    let products= await getProductsbyuserId(context,parent);
+    let products = await getProductsbyuserId(context, parent);
     return products;
-},
-}
+  },
+  async ancestorId(parent, args, context, info) {
+    return parent.ancestors[0];
+  },
+  async parentId(parent, args, context, info) {
+    return encodeOpaqueId("reaction/product", parent.ancestors[0]);
+    //encode( encodeOpaqueId(parent.ancestors[0]))
+  },
+};
