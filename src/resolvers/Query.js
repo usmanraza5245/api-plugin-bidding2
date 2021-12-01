c3506f407f020eb0158aa2b9c5d5f2d6428aecb7
husamaz1994 <11besehzulfiqar@seecs.edu.pk>
2021-12-01 16:43:55 +0500

    canFoloow change to followers

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -95,7 +95,7 @@ export default {
       profilePhoto: account.profile.picture,
       followerData:account.follower,
       followingData:account.following,
-      canFollow:(account.following&&account.following.indexOf(context.userId)==-1)||account.following==undefined?true:false,
+      canFollow:(account.follower&&account.follower.indexOf(context.userId)==-1)||account.follower==undefined?true:false,
       isVerified:account.profile.identityVerified?true:false
     };
   },
